cc6a827f9675ef5ec9a8b3e256c24684e27b3f0b
Cheong Yip <wingy3181@gmail.com>
2016-05-19 08:23:12 +1000

    [os] Add my own custom directories to be created initially

--- src/os/create_directories.sh
+++ src/os/create_directories.sh
@@ -4,7 +4,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
     && source "utils.sh"
 
 declare -a DIRECTORIES=(
+    "$HOME/Development/code/bitbucket"
+    "$HOME/Development/code/github"
+    "$HOME/Development/code/playground"
+    "$HOME/Documents/ebook"
     "$HOME/Downloads/torrents"
+    "$HOME/Videos/_to-organise"
+    "$HOME/Videos/_to-watch-later"
 )
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
