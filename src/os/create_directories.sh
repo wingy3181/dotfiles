61c373c0ba73dd9c957e7933656bb8a818ff141c
Cheong Yip <wingy3181@gmail.com>
2016-05-25 13:33:56 +1000

    [os] Add directories for default Screenshot location and Transmission download locations

--- src/os/create_directories.sh
+++ src/os/create_directories.sh
@@ -4,11 +4,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
     && source "utils.sh"
 
 declare -a DIRECTORIES=(
+    "$HOME/Desktop/Screenshots"
     "$HOME/Development/code/bitbucket"
     "$HOME/Development/code/github"
     "$HOME/Development/code/playground"
     "$HOME/Documents/ebook"
-    "$HOME/Downloads/torrents"
+    "$HOME/Downloads/torrents/complete"
+    "$HOME/Downloads/torrents/incomplete"
     "$HOME/Videos/_to-organise"
     "$HOME/Videos/_to-watch-later"
 )
