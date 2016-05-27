b12ff6feeacde24b7d1c21b7c67e2c7c1873666f
Cătălin Mariș <alrraa@gmail.com>
2016-05-27 22:18:34 +0200

    [osx] Reorganize the content from `installs/`

--- src/os/os_x/installs/main.sh
+++ src/os/os_x/installs/main.sh
@@ -25,245 +25,34 @@ main() {
     # - All variables declared local will not be shared.
     # shellcheck disable=SC2034
     if $skipQuestions; then
-        previousInstallApplicationConfirmationReply="a"
+        export previousInstallApplicationConfirmationReply="a"
     else
-        previousInstallApplicationConfirmationReply=""
+        export previousInstallApplicationConfirmationReply=""
     fi
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     ./install_xcode.sh
 
-    print_in_green "\n  ---\n\n"
-
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    print_in_green "\n  ---\n\n"
     ./install_homebrew.sh
-    ./update_and_upgrade.sh
+    brew_update
+    brew_upgrade
 
     print_in_green "\n  ---\n\n"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Bash" "bash"
-    brew_install_with_confirmation "Bash Completion 2" "bash-completion2" "homebrew/versions"
-    ./change_default_bash_version.sh
-
-    print_in_green "\n  ---\n\n"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Alfred" "alfred" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Android File Transfer" "android-file-transfer" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "AppCleaner" "appcleaner" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Atom" "atom" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Bartender 2" "bartender" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Cheatsheet" "cheatsheet" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Chrome" "google-chrome" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Chrome Canary" "google-chrome-canary" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Disk Inventory X" "disk-inventory-x" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    brew_install_with_confirmation "Dropbox" "dropbox" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "FFmpeg" "ffmpeg"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Firefox" "firefox" "caskroom/cask" "cask"
-    brew_install_with_confirmation "Firefox Developer" "firefoxdeveloperedition" "caskroom/versions" "cask"
-    brew_install_with_confirmation "Firefox Nightly" "firefoxnightly" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Gas Mask" "gas-mask" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "GIMP" "lisanet-gimp" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Git" "git"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "GPG Tools" "gpgtools" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Handbrake" "handbrake" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Helium by Jaden Geller" "jadengeller-helium" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "ImageAlpha" "imagealpha" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "ImageMagick" "imagemagick --with-webp"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "ImageOptim" "imageoptim" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Intellij IDEA" "intellij-idea" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "iTerm2" "iterm2" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Java 6" "java6" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Java 7" "java7" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Java 8" "java" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "LICEcap" "licecap" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Opera" "opera" "caskroom/cask" "cask"
-    brew_install_with_confirmation "Opera Beta" "opera-beta" "caskroom/versions" "cask"
-    brew_install_with_confirmation "Opera Developer" "opera-developer" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    #https://github.com/sindresorhus/quick-look-plugins
-    brew_install_with_confirmation "Quick Look plugins" "qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # `Safari Technology Preview` requires OS X 10.11.4 or later
-    # https://github.com/alrra/dotfiles/issues/26
-
-    if is_supported_version "$(get_os_version)" "10.11.4"; then
-        brew_install_with_confirmation "Safari Technology Preview" "safari-technology-preview" "caskroom/versions" "cask"
-    fi
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "ShellCheck" "shellcheck"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Snagit" "snagit" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "SourceTree" "sourcetree" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Spectacle" "spectacle" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Spotify" "spotify" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "TeamViewer" "teamviewer" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "tmux" "tmux"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Tor Browser" "torbrowser" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "tree" "tree"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Transmission" "transmission" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Unarchiver" "the-unarchiver" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # https://github.com/Homebrew/homebrew-core/blob/master/Formula/vim.rb
-    brew_install_with_confirmation "Vim" "vim --override-system-vi"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "VLC" "vlc" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Web Font Tools: TTF/OTF → WOFF (Zopfli)" "sfnt2woff-zopfli" "bramstein/webfonttools"
-    brew_install_with_confirmation "Web Font Tools: TTF/OTF → WOFF" "sfnt2woff" "bramstein/webfonttools"
-    brew_install_with_confirmation "Web Font Tools: WOFF2" "woff2" "bramstein/webfonttools"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "WebStorm" "webstorm" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "WebKit" "webkit-nightly" "caskroom/versions" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "XtraFinder" "xtrafinder" "caskroom/cask" "cask"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "z" "z"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    brew_install_with_confirmation "Zopfli" "zopfli"
+    ./install_bash.sh
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     print_in_green "\n  ---\n\n"
+    ./install_apps.sh
 
-    ./cleanup.sh
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_in_green "\n  ---\n\n"
+    brew_cleanup
 
 }
 
--- src/os/os_x/installs/utils.sh
+++ src/os/os_x/installs/utils.sh
@@ -5,33 +5,21 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-brew_install_with_confirmation() {
+brew_cleanup() {
 
-    declare -r FORMULA_READABLE_NAME="$1"
+    # By default Homebrew does not uninstall older versions
+    # of formulas so, in order to remove them, `brew cleanup`
+    # needs to be used:
+    #
+    # https://github.com/Homebrew/brew/blob/496fff643f352b0943095e2b96dbc5e0f565db61/share/doc/homebrew/FAQ.md#how-do-i-uninstall-old-versions-of-a-formula
 
-    # Install application if previous confirmation reply was 'install all'
-    if [[ "$previousInstallApplicationConfirmationReply" =~ ^[aA]$ ]]; then
-        brew_install "$@"
-    # DON'T install application if previous confirmation reply was 'skip all'
-    elif [[ "$previousInstallApplicationConfirmationReply" =~ ^[sS]$ ]]; then
-        print_warning "$FORMULA_READABLE_NAME (not installed)"
-    # Otherwise, prompt user and install application based on reply
-    else
-        local installApplicationConfirmationReply=""
-        # Until a valid reply has been entered, keep prompting user
-        while [[ -z "$installApplicationConfirmationReply" || "$installApplicationConfirmationReply" =~ ^[^yYnNaAsS]$ ]]; do
-            ask_for_install_application_confirmation "Do you want to install $FORMULA_READABLE_NAME?"
-            installApplicationConfirmationReply="$(get_answer)"
-        done
+    execute \
+        "brew cleanup" \
+        "brew (cleanup)"
 
-        if answer_is_yes || answer_is_yes_to_all; then
-            brew_install "$@"
-        elif answer_is_no || answer_is_skip_all; then
-            print_warning "$FORMULA_READABLE_NAME (not installed)"
-        fi
-
-        previousInstallApplicationConfirmationReply="$(get_answer)"
-    fi
+    execute \
+        "brew cask cleanup" \
+        "brew cask (cask cleanup)"
 
 }
 
@@ -82,6 +70,52 @@ brew_install() {
 
 }
 
+brew_install_with_confirmation() {
+
+    declare -r FORMULA_READABLE_NAME="$1"
+
+    # Install application if previous confirmation reply was 'install all'
+    if [[ "$previousInstallApplicationConfirmationReply" =~ ^[aA]$ ]]; then
+        brew_install "$@"
+    # DON'T install application if previous confirmation reply was 'skip all'
+    elif [[ "$previousInstallApplicationConfirmationReply" =~ ^[sS]$ ]]; then
+        print_warning "$FORMULA_READABLE_NAME (not installed)"
+    # Otherwise, prompt user and install application based on reply
+    else
+        local installApplicationConfirmationReply=""
+        # Until a valid reply has been entered, keep prompting user
+        while [[ -z "$installApplicationConfirmationReply" || "$installApplicationConfirmationReply" =~ ^[^yYnNaAsS]$ ]]; do
+            ask_for_install_application_confirmation "Do you want to install $FORMULA_READABLE_NAME?"
+            installApplicationConfirmationReply="$(get_answer)"
+        done
+
+        if answer_is_yes || answer_is_yes_to_all; then
+            brew_install "$@"
+        elif answer_is_no || answer_is_skip_all; then
+            print_warning "$FORMULA_READABLE_NAME (not installed)"
+        fi
+
+        previousInstallApplicationConfirmationReply="$(get_answer)"
+    fi
+    
+}
+
 brew_tap() {
     brew tap "$1" &> /dev/null
+}
+
+brew_update() {
+
+    execute \
+        "brew update" \
+        "brew (update)"
+
+}
+
+brew_upgrade() {
+
+    execute \
+        "brew upgrade --all" \
+        "brew (upgrade)"
+
 }
