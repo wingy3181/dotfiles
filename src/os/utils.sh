3050a5a9a44a9d73c923861811d193eb90ebdec0
Cheong Yip <wingy3181@gmail.com>
2016-06-15 16:04:45 +1000

    [shell] Fix issue with reloading/source ~/.bash_profile

--- src/os/utils.sh
+++ src/os/utils.sh
@@ -116,7 +116,6 @@ execute() {
     #   If parameter is unset or null, the expansion of word is substituted.
     #   Otherwise, the value of parameter is substituted.
     exitCode=$?
-    print_in_purple "$exitCode"
 
     if [ $exitCode -ne 0 ]; then
         print_error_stream "↳ ERROR:" < "$tmpFile"
