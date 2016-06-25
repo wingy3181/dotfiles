bb0faf58181c3a7f1de84676c3a11c65cd20c1ac
Cheong Yip <wingy3181@gmail.com>
2016-06-25 10:38:23 +1000

    [os] Add more directories to create at start to organise ebooks and videos

--- src/os/create_directories.sh
+++ src/os/create_directories.sh
@@ -8,11 +8,14 @@ declare -a DIRECTORIES=(
     "$HOME/Development/code/bitbucket"
     "$HOME/Development/code/github"
     "$HOME/Development/code/playground"
-    "$HOME/Documents/ebook"
+    "$HOME/Documents/ebook/_to-organise"
+    "$HOME/Documents/ebook/_to-read-later"
+    "$HOME/Documents/ebook/_read"
     "$HOME/Downloads/torrents/complete"
     "$HOME/Downloads/torrents/incomplete"
     "$HOME/Videos/_to-organise"
     "$HOME/Videos/_to-watch-later"
+    "$HOME/Videos/_watched"
 )
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
