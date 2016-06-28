ce792d3e463b51902566de22d95b0ab45448a50e
Cătălin Mariș <alrraa@gmail.com>
2016-06-28 21:06:25 +0300

    [macos] Enable `Secure Keyboard Entry` in Terminal
    From https://help.apple.com/terminal/mac/10.11/index.html?localePath=en.lproj#/trml109:
    
     " Prevent unwanted detection of keystrokes
    
       Secure keyboard entry can prevent other apps on your computer or
       the network from detecting and recording what you type in Terminal. "
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    See also: https://security.stackexchange.com/q/47749
    
    Thanks @kdeldycke!

--- src/os/preferences/macos/terminal.sh
+++ src/os/preferences/macos/terminal.sh
@@ -10,7 +10,10 @@ print_in_purple "\n  Terminal\n\n"
 execute "defaults write com.apple.terminal FocusFollowsMouse -string true" \
     "Make the focus automatically follow the mouse"
 
-execute "defaults write com.apple.terminal S -array 4" \
+execute "defaults write com.apple.terminal SecureKeyboardEntry -bool true" \
+    "Enable 'Secure Keyboard Entry'"
+
+execute "defaults write com.apple.terminal StringEncodings -array 4" \
     "Only use UTF-8"
 
 execute "./set_terminal_theme.applescript" \
