2c99dcc56fea5c712b3b91acd8d1cd045e3fac49
Cătălin Mariș <alrraa@gmail.com>
2016-07-01 21:18:34 +0200

    [os] Show spinner while cmds are being executed
    This addition not only provides the user with visual feedback,
    but also eliminates the possibility of jobs timing out because
    of the lack¹ of log output.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    ¹ https://docs.travis-ci.com/user/customizing-the-build/#Build-Timeouts

--- src/os/utils.sh
+++ src/os/utils.sh
@@ -91,10 +91,36 @@ cmd_exists() {
     command -v "$1" &> /dev/null
 }
 
+kill_all_subprocesses() {
+
+    local i=""
+
+    for i in $(jobs -p); do
+        kill "$i"
+        wait "$i" &> /dev/null
+    done
+
+}
+
 execute() {
 
-    local tmpFile="$(mktemp /tmp/XXXXX)"
+    local -r CMDS="$1"
+    # See http://unix.stackexchange.com/questions/122845/using-a-b-for-variable-assignment-in-scripts
+    # ${parameter:-word}
+    #   If parameter is unset or null, the expansion of word is substituted.
+    #   Otherwise, the value of parameter is substituted.
+    local -r MSG="${2:-$1}"
+    local -r TMP_FILE="$(mktemp /tmp/XXXXX)"
+
     local exitCode=0
+    local cmdsPID=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # If the current process is ended,
+    # also end all its subprocesses
+
+    set_trap "EXIT" "kill_all_subprocesses"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -103,25 +129,43 @@ execute() {
     # and its exit status returned as the exit status of eval. If there are no arguments or only empty
     # arguments, the return status is zero.
     if [[ $DOTFILES_DEBUG_MODE = "true" ]]; then
-        eval "$1"
+        eval "$CMDS"
     else
-        eval "$1" \
+        eval "$CMDS" \
             &> /dev/null \
-            2> "$tmpFile"
+            2> "$TMP_FILE" &
     fi
 
-    print_result $? "${2:-$1}"
-    # See http://unix.stackexchange.com/questions/122845/using-a-b-for-variable-assignment-in-scripts
-    # ${parameter:-word}
-    #   If parameter is unset or null, the expansion of word is substituted.
-    #   Otherwise, the value of parameter is substituted.
+    cmdsPID=$!
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Show a spinner if the commands
+    # require more time to complete
+
+    show_spinner "$cmdsPID" "$CMDS" "$MSG"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait for the commands to no longer be executing
+    # in the background, and then get their exit code
+
+    wait "$cmdsPID" &> /dev/null
     exitCode=$?
 
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Print output based on what happened
+
+    print_result $exitCode "$MSG"
+
     if [ $exitCode -ne 0 ]; then
-        print_error_stream "↳ ERROR:" < "$tmpFile"
+        print_error_stream < "$TMP_FILE"
     fi
 
-    rm -rf "$tmpFile"
+    rm -rf "$TMP_FILE"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     return $exitCode
 
@@ -235,7 +279,7 @@ print_error_stream() {
     # $1 : error prefix before each line of text
     # $2 : line of text from error output of command (called from 'execute' function)
     while read -r line; do
-        print_error "$1 $line"
+        print_error "↳ ERROR: $line"
     done
 }
 
@@ -347,6 +391,13 @@ print_warning() {
     print_in_yellow "  [!] $1\n"
 }
 
+set_trap() {
+
+    trap -p "$1" | grep "$2" &> /dev/null \
+        || trap '$2' "$1"
+
+}
+
 skip_questions() {
 
      while :; do
@@ -358,5 +409,115 @@ skip_questions() {
     done
 
     return 1
+
+}
+
+show_spinner() {
+
+    local -r FRAMES='/-\|'
+
+    # shellcheck disable=SC2034
+    local -r NUMBER_OR_FRAMES=${#FRAMES}
+
+    local -r CMDS="$2"
+    local -r MSG="$3"
+    local -r PID="$1"
+
+    local frameText=""
+    local frameTextLength=0
+    local i=0
+    local j=0
+    local numberOfLinesToBeCleared=0
+    local terminalWindowWidth=0
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # For commands that require sudo, if the password needs to be
+    # provided, wait for the user to provide it before showing the
+    # actual spinner
+    #
+    # (this is kinda hacky, but yeah...)
+
+    if printf "%s" "$CMDS" | grep "sudo" &>/dev/null; then
+        while kill -0 "$PID" &>/dev/null \
+                && ! sudo -n true &> /dev/null; do
+            sleep 0.2
+        done
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Display spinner while the commands are being executed
+
+    while kill -0 "$PID" &>/dev/null; do
+
+        frameText="  [${FRAMES:i++%NUMBER_OR_FRAMES:1}] $MSG"
+        numberOfLinesToBeCleared=1
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Print frame text
+
+        printf "%s" "$frameText"
+        sleep 0.2
+
+        # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        # Clear frame text
+
+        # Notes:
+        #
+        #  * After the content surpasses the initial terminal height
+        #    (the content forces the scroll), `tput sc` (save the cursor
+        #    position) and `tput rc` (restore the cursor position) will
+        #    no longer be reliable
+        #
+        # * `tput ed` (clear to end of screen) seems to also not always
+        #    be reliable
+        #
+        # So, in order to work around the shortcomings described above,
+        # the clearing of the previous printed content will have to be
+        # done "manually".
+
+        # The content may not fit into a single line so there is a
+        # need to determine on how many lines it is printed on and
+        # clear every single one of those lines
+
+        terminalWindowWidth=$(tput cols)
+        frameTextLength=${#frameText}
+
+        if [ "$terminalWindowWidth" -lt "$frameTextLength" ]; then
+            numberOfLinesToBeCleared=$(( numberOfLinesToBeCleared + ( frameTextLength / terminalWindowWidth ) ))
+        fi
+
+        for j in $(seq 1 $numberOfLinesToBeCleared); do
+
+            # Clear current line
+
+            tput el     # Clear to end of line
+            tput el1    # Clear to beginning of line
+
+            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            # The following line is just so that things look ok on
+            # the Travis CI site. The line isn't really needed, but
+            # also it doesn't do any harm. However, without it all
+            # the frames of the spinner will just be displayed one
+            # after the other in a single line.
+
+            printf "\r"
+
+            # - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+            # Move up one line if the line containing the starting
+            # position of the content has not been reached
+
+            if [ "$j" -lt "$numberOfLinesToBeCleared" ]; then
+                tput cuu1
+            fi
+
+        done
+
+    done
 
 }
