5fee1e3e6200b5fc5d6cdeae011d140052417a51
Cheong Yip <wingy3181@gmail.com>
2018-04-26 08:59:54 +1000

    [npm] Update to use npm package @angular/cli instead of angular-cli

--- src/os/install/npm_packages.sh
+++ src/os/install/npm_packages.sh
@@ -6,7 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 declare -r -a NPM_PACKAGES=(
-    "angular-cli"
+    "@angular/cli"
     "babel-cli"
     "browser-sync"
     "bower"
