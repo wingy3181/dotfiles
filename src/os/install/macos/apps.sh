6ad222d90b56789e46add50b486a03445bcd8cdb
Cheong Yip <wingy3181@gmail.com>
2023-11-19 07:46:03 +1100

    ➕ Install Brave Browser

--- src/os/install/macos/apps.sh
+++ src/os/install/macos/apps.sh
@@ -30,6 +30,10 @@ brew_install_with_confirmation "bat" "bat"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+brew_install_with_confirmation "Brave Browser" "brave-browser" "caskroom/cask" "cask"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 brew_install_with_confirmation "Cheatsheet" "cheatsheet" "caskroom/cask" "cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
