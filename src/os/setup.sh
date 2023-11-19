fccffa6dee7bfa205775e97146c2922fbcc13b81
Cheong Yip <wingy3181@gmail.com>
2023-11-19 16:53:02 +1100

    🔀 Add 'Silicon' as optional application to install

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -360,6 +360,7 @@ main() {
     print_optional_info "Install Microsoft Remote Desktop from App Store (https://itunes.apple.com/au/app/microsoft-remote-desktop-10/id1295203466?mt=12)"
     print_optional_info "Install Presentify from App Store (https://apps.apple.com/app/id1507246666 and https://presentify.compzets.com/)"
     print_optional_info "Install pygments: 'sudo easy_install -U Pygments'"
+    print_optional_info "Install Silicon (https://github.com/DigiDNA/Silicon)"
     print_optional_info "Install Soda Player (https://www.sodaplayer.com/)"
     print_optional_info "Install Synergy"
     print_optional_info "Install Java 7, 11 from Oracle (https://www.oracle.com/downloads/ and https://www.oracle.com/technetwork/java/javase/downloads/index.html)"
