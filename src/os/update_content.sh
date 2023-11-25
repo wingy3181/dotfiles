5f371687989f8197b9d911c79329b8358bf7be11
Cheong Yip <wingy3181@gmail.com>
2023-11-26 09:05:40 +1100

    ♻️ Execute get_os only once in bash_prompt
    Fix linting issues
    SC2181 - Check exit code directly with e.g. 'if mycmd;',
             not indirectly with $?.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    Ref: https://github.com/koalaman/shellcheck/wiki/SC2181
    
    See https://github.com/alrra/dotfiles/commit/98b25d2746d6adb7a571f791186a3d4a0791b962

--- src/os/update_content.sh
+++ src/os/update_content.sh
@@ -10,28 +10,25 @@ main() {
     ssh -T git@github.com &> /dev/null
 
     if [ $? -ne 1 ]; then
-        ./set_github_ssh_key.sh
+        ./set_github_ssh_key.sh \
+            || return 1
     fi
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    print_in_purple "\n\n * Update content\n\n"
 
-    if [ $? -eq 0 ]; then
+    ask_for_confirmation "Do you want to update the content from the 'dotfiles' directory?"
 
-        print_in_purple "\n\n * Update content\n\n"
+    if answer_is_yes; then
 
-        ask_for_confirmation "Do you want to update the content from the 'dotfiles' directory?"
+        git fetch --all 1> /dev/null \
+            && git reset --hard origin/master 1> /dev/null \
+            && git clean -fd 1> /dev/null
 
-        if answer_is_yes; then
-
-            git fetch --all 1> /dev/null \
-                && git reset --hard origin/master 1> /dev/null \
-                && git clean -fd 1> /dev/null
-
-            print_result $? "Update content"
-        fi
-
+        print_result $? "Update content"
     fi
+
 }
 
 main
