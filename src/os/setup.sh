2a76f89e70c7e6ecf7201187b20297f6abb10193
Cheong Yip <wingy3181@gmail.com>
2023-11-26 17:49:57 +1100

    ♻️ Tweak the get_os, get_os_version, and verify_os functions
    * Make the `get_os` and `get_os_version` functions use the
      standard `/etc/os-release`¹ file on Linux.
    
    * Make the `verify_os` functions use the helper `get_os` and
      `get_os_version` functions.
    
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    
    ¹ http://0pointer.de/blog/projects/os-release
    
    Close: #72
    
    See https://github.com/alrra/dotfiles/commit/1c3a27fd626a41d6e8ba3c9619dc93781dcddd0c

--- src/os/setup.sh
+++ src/os/setup.sh
@@ -168,19 +168,15 @@ verify_os() {
 
     declare -r MINIMUM_MACOS_VERSION="10.10"
 
-    local os_name=""
-    local os_version=""
+    local os_name="$(get_os)"
+    local os_version="$(get_os_version)"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # Check if the OS is `macOS` and
     # it's above the required version.
 
-    os_name="$(uname -s)"
-
-    if [ "$os_name" == "Darwin" ]; then
-
-        os_version="$(sw_vers -productVersion)"
+    if [ "$os_name" == "macos" ]; then
 
         if is_supported_version "$os_version" "$MINIMUM_MACOS_VERSION"; then
             return 0
