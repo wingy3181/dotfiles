5bd26f445ed1826a91f70609a46a8382d0246048
Cheong Yip <wingy3181@gmail.com>
2023-12-08 20:32:25 +1100

    ➕ Update 4K Video Downloader to 4k Video Downloader Plus

--- src/os/installs/macos/downloaders.sh
+++ src/os/installs/macos/downloaders.sh
@@ -8,7 +8,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Downloaders\n\n"
 
-brew_install_with_confirmation "4K Video Downloader" "4k-video-downloader" "--cask"
+brew_install_with_confirmation "4K Video Downloader" "4k-video-downloaderplus" "--cask"
 brew_install_with_confirmation "Transmission" "transmission" "--cask"
 brew_install_with_confirmation "sabnzbd" "sabnzbd" "--cask"
 brew_install_with_confirmation "yt-dlp" "yt-dlp"
