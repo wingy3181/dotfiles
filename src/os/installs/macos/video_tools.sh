7c3ab16965a967b4a2cd8c31a410575e2d79a31f
Cheong Yip <wingy3181@gmail.com>
2024-02-09 08:33:54 +1100

    ✨ Add GoPro Quik

--- src/os/installs/macos/video_tools.sh
+++ src/os/installs/macos/video_tools.sh
@@ -12,6 +12,7 @@ brew_install_with_confirmation "VLC" "vlc" "--cask"
 
 brew_install_with_confirmation "FFmpeg" "ffmpeg"
 # https://gopro.com/en/au/shop/quik-app-video-photo-editor
+mas_install_with_confirmation  "GoPro Quik Video Photo Editor" "561350520"
 # https://gopro.com/en/au/info/gopro-player
 mas_install_with_confirmation  "GoPro Player + ReelSteady" "1460836908"
 brew_install_with_confirmation "Handbrake" "handbrake" "--cask"
