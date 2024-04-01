6f1d5d9099006acd65784368512b5911413035f0
Cheong Yip <wingy3181@gmail.com>
2024-04-01 16:29:31 +1100

    ✨ Add DaVinci Resolve install via mas

--- src/os/installs/macos/video_tools.sh
+++ src/os/installs/macos/video_tools.sh
@@ -8,14 +8,14 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Video Tools\n\n"
 
-brew_install_with_confirmation "VLC" "vlc" "--cask"
-
+mas_install_with_confirmation  "DaVinci Resolve" "571213070"
 brew_install_with_confirmation "FFmpeg" "ffmpeg"
 # https://gopro.com/en/au/shop/quik-app-video-photo-editor
 mas_install_with_confirmation  "GoPro Quik Video Photo Editor" "561350520"
 # https://gopro.com/en/au/info/gopro-player
 mas_install_with_confirmation  "GoPro Player + ReelSteady" "1460836908"
 brew_install_with_confirmation "Handbrake" "handbrake" "--cask"
+brew_install_with_confirmation "VLC" "vlc" "--cask"
 
 
 
