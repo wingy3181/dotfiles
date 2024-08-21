daa4bd5a6ab56b8e66189b727cdd8a1c999e5559
Cheong Yip <wingy3181@gmail.com>
2024-08-21 18:34:15 +1000

    🐛 Fix brew installs for canary/nightly/beta versions

--- src/os/installs/macos/browsers.sh
+++ src/os/installs/macos/browsers.sh
@@ -25,7 +25,7 @@ brew_install_with_confirmation "Brave Browser" "brave-browser" "--cask"
 printf "\n"
 
 brew_install_with_confirmation "Chrome" "google-chrome" "--cask"
-brew_install_with_confirmation "Chrome Canary" "google-chrome@canary" "--cask" "homebrew/cask-versions"
+brew_install_with_confirmation "Chrome Canary" "google-chrome@canary" "--cask"
 brew_install_with_confirmation "Chromium" "chromium" "--cask --no-quarantine"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -33,8 +33,8 @@ brew_install_with_confirmation "Chromium" "chromium" "--cask --no-quarantine"
 printf "\n"
 
 brew_install_with_confirmation "Firefox" "firefox" "--cask"
-brew_install_with_confirmation "Firefox Developer" "firefox@developer-edition" "--cask" "homebrew/cask-versions"
-brew_install_with_confirmation "Firefox Nightly" "firefox@nightly" "--cask" "homebrew/cask-versions"
+brew_install_with_confirmation "Firefox Developer" "firefox@developer-edition" "--cask"
+brew_install_with_confirmation "Firefox Nightly" "firefox@nightly" "--cask"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -49,7 +49,7 @@ brew_install_with_confirmation "Microsoft Edge" "microsoft-edge" "--cask"
 
 if is_supported_version "$(get_os_version)" "10.11.4"; then
     printf "\n"
-    brew_install_with_confirmation "Safari Technology Preview" "safari-technology-preview" "--cask" "homebrew/cask-versions"
+    brew_install_with_confirmation "Safari Technology Preview" "safari-technology-preview" "--cask"
 fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
