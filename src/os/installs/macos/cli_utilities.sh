3ddc479b68fe740d7095761c9cdd92e3348a9731
Cheong Yip <wingy3181@gmail.com>
2024-09-08 17:01:14 +1000

    🔧 add delta to prettier diffs + update git aliases to use delta
    Ref: https://twitter.com/rauchg/status/1831421759666676165
    
    Closes: #20

--- src/os/installs/macos/cli_utilities.sh
+++ src/os/installs/macos/cli_utilities.sh
@@ -9,6 +9,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   CLI Utilities\n\n"
 
 brew_install_with_confirmation "bat" "bat"
+brew_install_with_confirmation "delta" "delta"
 brew_install_with_confirmation "jq" "jq"
 # zoxide is optionally dependent on fzf
 brew_install_with_confirmation "fzf" "fzf"
