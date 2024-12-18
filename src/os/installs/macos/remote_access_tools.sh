67f6d66a6acc56ae728cbeb3170e3592b5ebd483
Cheong Yip <wingy3181@gmail.com>
2024-12-19 07:57:59 +1100

    🔧 Replace Microsoft Remote Desktop from Brewfile and remote_access_tools script with Windows App due to rebranding

--- src/os/installs/macos/remote_access_tools.sh
+++ src/os/installs/macos/remote_access_tools.sh
@@ -11,7 +11,6 @@ print_in_purple "\n   Remote Access Tools\n\n"
 # https://support.apple.com/en-au/guide/remote-desktop/welcome/mac
 # https://apps.apple.com/au/app/apple-remote-desktop/id409907375?mt=12
 mas_install_with_confirmation  "Apple Remote Desktop" "409907375"
-brew_install_with_confirmation "Microsoft Remote Desktop" "microsoft-remote-desktop" "--cask"
 # https://github.com/Genymobile/scrcpy
 # https://developer.android.com/studio/command-line/adb.html#wireless
 brew_install_with_confirmation "scrcpy" "scrcpy"
@@ -22,6 +21,7 @@ brew_install_with_confirmation "sshrc" "ikuwow/ikuwow-sshrc/sshrc"
 # Re-assess later
 #brew_install_with_confirmation "TeamViewer" "teamviewer" "--cask"
 brew_install_with_confirmation "vysor.io" "vysor" "--cask"
-
+# https://learn.microsoft.com/en-us/windows-app/
+brew_install_with_confirmation "Windows App" "windows-app" "--cask"
 
 
