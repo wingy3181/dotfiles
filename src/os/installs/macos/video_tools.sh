26f6085a221a47183abd7d804ec4721f22d126c3
Cheong Yip <wingy3181@gmail.com>
2024-12-19 07:47:51 +1100

    🔧 Remove GoPro Quik from Brewfile and video_tools script as EOL
    See https://community.gopro.com/s/article/Quik-Mac-Info-Troubleshooting?language=en_US

--- src/os/installs/macos/video_tools.sh
+++ src/os/installs/macos/video_tools.sh
@@ -10,8 +10,6 @@ print_in_purple "\n   Video Tools\n\n"
 
 mas_install_with_confirmation  "DaVinci Resolve" "571213070"
 brew_install_with_confirmation "FFmpeg" "ffmpeg"
-# https://gopro.com/en/au/shop/quik-app-video-photo-editor
-mas_install_with_confirmation  "GoPro Quik Video Photo Editor" "561350520"
 # https://gopro.com/en/au/info/gopro-player
 mas_install_with_confirmation  "GoPro Player + ReelSteady" "1460836908"
 brew_install_with_confirmation "Handbrake" "handbrake" "--cask"
